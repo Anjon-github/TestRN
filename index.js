0c74f3340c9262c497a61b0b73063127ab31196c
Anjon-github <xionghoumiao@foxmail.com>
2018-09-05 23:36:09 +0800

    1.init rn project

---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import Network from './app/Network';
+
+// AppRegistry.registerComponent('TestRN', () => App);
+
+AppRegistry.registerComponent('TestRN', () => Network);
